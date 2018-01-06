b681977bca288f88f4cd065d68e03818485b874c
Shane Moyo <smoyo@pdx.edu>
2018-01-06 12:54:58 -0800

    sigin in with credentials test status:passing

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -4,41 +4,48 @@ const mongoose = require('mongoose');
 const User = require('../../lib/models/user');
 const tokenService = require('../../lib/utils/token-service');
 
+
 describe('Auth API', () => {
   beforeEach(() => mongoose.connection.dropDatabase());
 
-  let token = null; 
-  beforeEach(()=>{
-      const user = new User({
-          email: 'test@test.com',
-          firstName: 'first name',
-          lastName: 'last name',
-          roles: ['admin']
-      });
-      user.generateHash('password');
-      return user.save()
-          .then(user => tokenService.sign(user))
-          .then(signed => token = signed );
-  });
-
   it('Should generate a token on signup', () => {
-    assert.ok(token);
+    return request.post('/api/auth')
+        .send({ 
+            email: 'test@test.com',
+            firstName: 'first name',
+            lastName: 'last name',
+            roles: ['admin'],
+            password: 'password'
+            
+        })
+        .then(({ body }) => assert.ok(body.token))
   });
 
   it('throws error if email already exists',() => {
     return request.post('/api/auth')
-        .send({
-          email: 'test@test.com',
-          firstName: 'first name',
-          lastName: 'last name',
-          roles: ['admin']
+        .send({ 
+            email: 'test@test.com',
+            firstName: 'first name',
+            lastName: 'last name',
+            roles: ['user'],
+            password: 'password'
         })
-        .then(
-            () => { throw new Error('Unexpected successful response'); },
-            err => {
-                assert.equal(err.status, 400);
-            }
-        );
+        .then(()=> {
+            return request.post('/api/auth')
+                .send({
+                    email: 'test@test.com',
+                    firstName: 'first name',
+                    lastName: 'last name',
+                    roles: ['user'],
+                    password: 'password'
+                })
+                .then(
+                    () => { throw new Error('Unexpected successful response'); },
+                    err => {
+                        assert.equal(err.status, 400);
+                    }
+                );
+        });
   });
 
   it('Should throw an error if password is not included', () => {
@@ -56,6 +63,28 @@ describe('Auth API', () => {
                 assert.equal(err.status, 400);
             }
         );       
-});
+  });
+
+  it('Should signin with valid credentials', () => {
+    return request.post('/api/auth')
+    .send({ 
+        email: 'test@test.com',
+        firstName: 'first name',
+        lastName: 'last name',
+        roles: ['user'],
+        password: 'password'
+    })
+    .then(()=> {
+    return request
+        .post('/api/auth/signin')
+        .send({ 
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .then(({ body }) => {
+            assert.isOk(body.token);
+        });   
+    });       
+  });
 
 })
