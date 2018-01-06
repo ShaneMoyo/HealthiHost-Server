e03cd64394378f04fea67d439a4cfe4b6a27d345
Shane Moyo <smoyo@pdx.edu>
2018-01-06 11:38:45 -0800

    should generate a token on signup status:passing

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -1,6 +1,6 @@
 const connect = require( '../../lib/connect');
 const mongoose = require( 'mongoose');
-const url = 'mongodb://localhost:27017/mothersmilk-test';
+const url = 'mongodb://localhost:27017/Healthihost-test';
 
 before(()=> connect(url));
 after(()=> mongoose.connection.close());
