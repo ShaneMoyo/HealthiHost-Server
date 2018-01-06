de5b070fcec76604444d89a9787f62414d208e56
Shane Moyo <smoyo@pdx.edu>
2018-01-06 13:43:08 -0800

    should validate a good appointment model status:passing

--- test/unit/appointment.js
+++ test/unit/appointment.js
@@ -1,23 +1,19 @@
 const assert = require('chai').assert; 
 const Appointment = require('../../lib/models/appointment');
 
+
+
 describe('Appointments API', () => {
+  //working on saving an appointment
   it('Should validate a good model', () => {
     const appointment = new Appointment({
       service: 'Massage', 
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true 
-      },
-      date: {
-        type: Date,
-        required: true
-      },
-      fulfilled: Boolean
+      user: '5a51421479c4f4070443f281',
+      date: '1970-01-01',
+      fulfilled: false
     });
     assert.equal(appointment.validateSync(), undefined)
   });
 
-  
+
 })
