932f591479a777d3ce1c238a0191675f0dfc463d
Shane Moyo <smoyo@pdx.edu>
2018-01-07 12:55:13 -0800

    should get all appointments with admin token test status:passing

--- test/e2e/appointments.test.js
+++ test/e2e/appointments.test.js
@@ -6,18 +6,31 @@ const assert = chai.assert;
 describe('Appointments API', () => {
 
   let token = '';
+  let admintoken = '';
   beforeEach(() => mongoose.connection.dropDatabase());
   beforeEach(() => {
-    return request.post('/api/auth/')
-      .send({
-        email: 'test@test.com',
-        firstName: 'first name',
-        lastName: 'last name',
-        roles: ['client'],
-        password: 'password'
-      })
-      .then(({ body })  => token = body )
-      })
+    return Promise.all([
+      request.post('/api/auth/')
+        .send({
+          email: 'test@test.com',
+          firstName: 'first name',
+          lastName: 'last name',
+          roles: ['client'],
+          password: 'password'
+        })
+        .then(({ body })  => token = body ),
+
+      request.post('/api/auth')
+        .send({
+          email: 'admin@admin.com',
+          firstName: 'first name',
+          lastName: 'last name',
+          roles: ['admin'],
+          password: 'password'
+        })
+        .then(({ body })  => adminToken = body ),
+      ]);
+    })
   
   const testAppointments = [
     {
@@ -48,6 +61,26 @@ describe('Appointments API', () => {
         assert.equal(savedAppointemnt.fulfilled, testAppointments[0].fulfilled);
       });
   });
+
+  it('Should get all saved appointments with admin role', () => {
+    const saveAppointemnts = testAppointments.map(appointment => {
+      return request.post('/api/appointments')
+        .set('Authorization', token)
+        .send(appointment)
+        .then(({ body: savedAppointemnt }) => savedAppointemnt)
+    });
+
+    return Promise.all(saveAppointemnts)
+      .then(savedAppointemnts => {
+        return request.get('/api/appointments')
+          .set('Authorization', adminToken)
+          .then(({ body: gotAppointemnts}) => {
+            gotAppointemnts = gotAppointemnts.sort((a, b) => a._id < b._id);
+            savedAppointemnts = saveAppointemnts.sort((a, b) => a._id < b._id);
+            assert.deepEqual(savedAppointemnts, gotAppointemnts);
+          });
+      });
+  })
 
 
 })
