f6d39ba5d42685609fd1ade0530adcf2ea145dd4
Shane Moyo <smoyo@pdx.edu>
2018-01-09 13:47:35 -0800

    add before each to users.test.js to for client and admin token

--- test/e2e/users.test.js
+++ test/e2e/users.test.js
@@ -3,4 +3,33 @@ const mongoose = require('mongoose');
 const request = require('./request');
 const assert = chai.assert;
 
-describe('Users API')
+describe('Users API', () => {
+
+  let token = '';
+  let admintoken = '';
+  beforeEach(() => mongoose.connection.dropDatabase());
+  beforeEach(() => {
+    return Promise.all([
+      request.post('/api/auth/')
+        .send({
+          email: 'test@test.com',
+          firstName: 'first name',
+          lastName: 'last name',
+          roles: ['client'],
+          password: 'password'
+        })
+        .then(({ body })  => token = body ),
+
+      request.post('/api/auth')
+        .send({
+          email: 'admin@admin.com',
+          firstName: 'first name',
+          lastName: 'last name',
+          roles: ['admin'],
+          password: 'password'
+        })
+        .then(({ body })  => adminToken = body ),
+      ]);
+    })
+  
+})
